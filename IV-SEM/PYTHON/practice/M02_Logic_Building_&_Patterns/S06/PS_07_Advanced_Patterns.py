b11c6cfde846b432555f0ab7dd1e93ab56d99b30
Ruthvik <addenkiruthvik2007@gmail.com>
2026-02-25 10:17:49 +0530

    Butterfly Pattern

--- IV-SEM/PYTHON/practice/M02_Logic_Building_&_Patterns/S06/PS_07_Advanced_Patterns.py
+++ IV-SEM/PYTHON/practice/M02_Logic_Building_&_Patterns/S06/PS_07_Advanced_Patterns.py
@@ -8,6 +8,7 @@ output:
 1 3 3 1
 1 4 6 4 1 
 ''' 
+'''
 n=int(input())
 for i in range(n):
     num=1
@@ -15,3 +16,22 @@ for i in range(n):
         print(num,end=" ")
         num=num*(i-j)//(j+1)
     print() 
+'''  
+'''
+2.Butterfly Pattern
+n=4
+output:
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
+''' 
+n=int(input())
+for i in range(1,n+1):
+    print("*"*i+" "*(2*(n-i))+"*"*i)
+for j in range(n,0,-1):
+    print("*"*j+" "*(2*(n-j))+"*"*j)

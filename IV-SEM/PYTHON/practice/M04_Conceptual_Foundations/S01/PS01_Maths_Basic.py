ab6dbd64ce2487d6d9e2f44106cb098abb4797ec
Ruthvik <addenkiruthvik2007@gmail.com>
2026-03-30 10:12:40 +0530

    PS01

---
+++ IV-SEM/PYTHON/practice/M04_Conceptual_Foundations/S01/PS01_Maths_Basic.py
@@ -0,0 +1,19 @@
+'''' 
+import math
+print(min([1,2,3,4]))
+print(max([1,2,3,4]))
+print(sum([1,2,3,4]))
+print(abs(12))#absolute_value
+print(abs(-10))
+#print(dir(math))
+print(math.factorial(5))
+#GCD of two numbers 
+'''
+'''
+8->1,2,4,8
+10->1,2,5,10 
+2 is the greatest common factor 
+''' 
+a=int(input())
+b=int(input())
+

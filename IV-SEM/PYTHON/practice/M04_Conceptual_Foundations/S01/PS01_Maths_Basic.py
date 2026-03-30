bb8f926903c8897cd809675bb52470a45585983b
Ruthvik <addenkiruthvik2007@gmail.com>
2026-03-30 10:13:40 +0530

    PS01

--- IV-SEM/PYTHON/practice/M04_Conceptual_Foundations/S01/PS01_Maths_Basic.py
+++ IV-SEM/PYTHON/practice/M04_Conceptual_Foundations/S01/PS01_Maths_Basic.py
@@ -14,6 +14,21 @@ print(math.factorial(5))
 10->1,2,5,10 
 2 is the greatest common factor 
 ''' 
+#sol1
+'''
 a=int(input())
 b=int(input())
-
+min_num=min(a,b)
+gcd=1
+for i in range(1,min_num+1):
+    if a%i==0 and b%i==0:
+        gcd=i 
+print(gcd)        
+'''
+#sol2
+a=int(input())
+b=int(input())
+gcd=1
+while b!=0:
+    a,b=b,a%b 
+print(a)
